8b4a5f969ba73eacbee109f3d36457cec5262f68
GaryYongYao <garyyongyao@gmail.com>
2018-02-01 19:58:04 +0800

    Initial commit

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID: '680769312444-6rpoobd1v7q2ji7dt9q32sl5q1frqf5u.apps.googleusercontent.com',
+  googleClientSecret: 'bTj6Eh88Lcscs_U6rezcB851',
+  mongoURI: 'mongodb://dev:password@ds137040.mlab.com:37040/emaily-dev-spextrex',
+  cookieKey: 'hdjakshdlALJSHKDsjdkfl',
+  stripePublishableKey: 'pk_test_7PVNq3NIg7ZXakRD1VuIPMrZ',
+  stripeSecretKey: 'sk_test_R0wJd1XknWH4GowwaRDLcTnK',
+  sendGridKey: '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',
+  redirectDomain: 'http://localhost:3000'
+};
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+require('./models/User');
+require('./models/Survey');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // express serve up production asset
+  // exp: main.js or main.css
+  app.use(express.static('client/build'));
+
+  //express serve up index.html
+  // if it doesnt recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
