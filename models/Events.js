49bbe890179229f116530e6bc9cf546b14f62fa3
garyyongyao <gary@superant.com.my>
2018-06-07 18:58:57 +0800

    Event Model Commit

--- models/Events.js
+++ models/Events.js
@@ -2,36 +2,31 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const eventSchema = new Schema({
-  //personal info
-  first_name: String,
-  last_name: String,
-  ic: String,
-  birthday: Date,
-  gender: String,
-  shirt_size: String,
-  //occupation
-  occupation: String,
-  school: String,
-  //contacts
-  phone: String,
-  email: String,
-  address: String,
-  current_address: String,
-  facebook: String,
-  instagram: String,
-  //in case of emergency
-  health: {
-    vegetarian: Boolean,
-    allergy: [String],
-    illness: String
-  },
-  emergency_contact: {
-    name: String,
-    relationship: String,
-    phone: String
-  },
-  //interest
-  volunteer_interest: [String],
+  //info
+  name          : String,
+  location      : String,
+  date          : Date,
+  desc          : String,
+  programme     : [{
+    time      : String,
+    activity  : String,
+    desc      : String
+  }],
+  volunteers    : [{ 
+    id    : {type: Schema.Types.ObjectId , ref: 'User'},
+    name  : String,
+    paid  : { type: Boolean, default: false}
+  }],
+  participants  : [{ 
+    name      : String,
+    age       : Number,
+    home      : String,
+    emergency_contact  : {
+      name: String,
+      relationship: String,
+      phone: String
+    }
+  }]
   //system info
   registerDate: { type: Date, default: Date.now },
 });
